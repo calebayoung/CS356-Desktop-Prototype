b22ebb016d800c8c2410c17050aa779c89f160d7
Caleb Young <caleb.a.young@gmail.com>
2021-02-11 21:27:03 -0700

    Add YMessage widget, final edits

--- script.js
+++ script.js
@@ -6,17 +6,15 @@ jQuery( $ => {
         $('.mybyu-grid__card-container').addClass('hidden');
         const targetType = $(this).text();
         $('.mybyu-grid__card-container').each(function () {
-            console.log('Data: ' + $(this).data('tab-type'));
-            console.log('Target type: ' + targetType);
-            console.log($(this).data('tab-type') == targetType);
             if ($(this).data('tab-type') == targetType) {
                 $(this).removeClass('hidden');
             }
         });
+        $('.mybyu-grid__tab-container').scrollTop(0);
     });
 
     $('.card').on('click', function() {
-        console.log('Navigation logic here . . .');
+        console.log('TODO: navigation logic here (if we want it)');
     });
 
     const searchTerms = {
